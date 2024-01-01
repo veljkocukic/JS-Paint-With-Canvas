a509c25fa50ead1582f43d74d568094f16c64470
veljkocukic <veljkocukic@gmail.com>
2024-01-01 21:11:00 +0100

    initial commit

---
+++ environment.js
@@ -0,0 +1,139 @@
+class Environment {
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.ctx = canvas.getContext("2d");
+    this.#addEventListeners();
+
+    this.segments = [];
+    this.rectangles = [];
+    this.circles = [];
+
+    this.style = {
+      color: "black",
+      width: 2,
+    };
+
+    this.state = {
+      drawingOn: false,
+      selectedTool: "pencil",
+      lastMouse: null,
+      currentlyAddingSegment: null,
+      currentlyAddingRectangle: null,
+      currentlyAddingCirlce: null,
+    };
+  }
+
+  draw() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.ctx.save();
+    this.ctx.fillStyle = "#fff";
+    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    this.ctx.restore();
+    for (const seg of this.segments) {
+      seg.draw(this.ctx);
+    }
+
+    for (const rec of this.rectangles) {
+      rec.draw(this.ctx);
+    }
+
+    for (const circ of this.circles) {
+      circ.draw(this.ctx);
+    }
+
+    this.state.currentlyAddingSegment &&
+      this.state.currentlyAddingSegment.draw(this.ctx);
+    this.state.currentlyAddingRectangle &&
+      this.state.currentlyAddingRectangle.draw(this.ctx);
+    this.state.currentlyAddingCirlce &&
+      this.state.currentlyAddingCirlce.draw(this.ctx);
+  }
+
+  changeWidth(width) {
+    this.style.width = width;
+  }
+
+  changeColor(color) {
+    this.style.color = color;
+  }
+
+  changeTool(tool) {
+    this.state.selectedTool = tool;
+  }
+
+  clearEnvironment() {
+    this.segments = [];
+    this.rectangles = [];
+  }
+
+  download() {
+    const link = document.createElement("a");
+    link.download = "image.jpg";
+    link.href = document.querySelector("canvas").toDataURL();
+    link.click();
+    link.remove();
+  }
+
+  #addEventListeners() {
+    this.canvas.addEventListener("mousedown", (e) => {
+      this.state.drawingOn = true;
+      this.state.lastMouse = new Point(e);
+    });
+
+    this.canvas.addEventListener("mousemove", (e) => {
+      const style = { width: this.style.width, color: this.style.color };
+
+      if (this.state.drawingOn) {
+        switch (this.state.selectedTool) {
+          case "circle":
+            this.state.currentlyAddingCirlce = new Circle(
+              this.state.lastMouse,
+              new Point(e),
+              style
+            );
+            break;
+          case "rectangle":
+            this.state.currentlyAddingRectangle = new Rectangle(
+              this.state.lastMouse,
+              new Point(e),
+              style
+            );
+            break;
+          case "segment":
+            this.state.currentlyAddingSegment = new Segment(
+              this.state.lastMouse,
+              new Point(e),
+              style
+            );
+            break;
+          case "pencil":
+            this.segments.push(
+              new Pencil(new Point(e), style).draw(this.state.lastMouse)
+            );
+            break;
+        }
+        if (this.state.selectedTool == "pencil") {
+          this.state.lastMouse = new Point(e);
+        }
+      }
+    });
+
+    this.canvas.addEventListener("mouseup", () => {
+      this.state.drawingOn = false;
+      this.state.lastMouse = null;
+      this.state.firstSegmentPoint = null;
+      if (this.state.selectedTool == "segment") {
+        this.segments.push(this.state.currentlyAddingSegment);
+        this.state.currentlyAddingSegment = null;
+      }
+      if (this.state.selectedTool == "rectangle") {
+        this.rectangles.push(this.state.currentlyAddingRectangle);
+        this.state.currentlyAddingRectangle = null;
+      }
+      if (this.state.selectedTool == "circle") {
+        this.rectangles.push(this.state.currentlyAddingCirlce);
+        this.state.currentlyAddingCirlce = null;
+      }
+    });
+  }
+}
---
+++ tools.js
@@ -0,0 +1,86 @@
+class Point {
+  constructor(e) {
+    this.x = e.offsetX;
+    this.y = e.offsetY;
+  }
+}
+
+class Segment {
+  constructor(p1, p2, style) {
+    this.p1 = p1;
+    this.p2 = p2;
+    this.style = style;
+  }
+
+  draw(ctx) {
+    ctx.save();
+    ctx.beginPath();
+    ctx.moveTo(this.p1.x, this.p1.y);
+    ctx.lineTo(this.p2.x, this.p2.y);
+    ctx.strokeStyle = this.style.color;
+    ctx.lineWidth = this.style.width;
+    ctx.stroke();
+    ctx.restore();
+  }
+}
+
+class Pencil {
+  constructor(mouse, style) {
+    this.mouse = mouse;
+    this.style = style;
+  }
+
+  draw(lastMouse) {
+    console.log(this.style);
+    if (lastMouse) {
+      const seg = new Segment(this.mouse, lastMouse, this.style);
+      return seg;
+    }
+  }
+}
+
+class Circle {
+  constructor(initialMouse, lastMouse, style) {
+    this.initialMouse = initialMouse;
+    this.style = style;
+    this.lastMouse = lastMouse;
+  }
+
+  draw(ctx) {
+    ctx.beginPath();
+    ctx.arc(
+      this.initialMouse.x,
+      this.initialMouse.y,
+      Math.hypot(
+        this.initialMouse.x - this.lastMouse.x,
+        this.initialMouse.y - this.lastMouse.y
+      ),
+      0,
+      Math.PI * 2
+    );
+    ctx.strokeStyle = this.style.color;
+    ctx.lineWidth = this.style.width;
+    ctx.stroke();
+  }
+}
+
+class Rectangle {
+  constructor(initialMouse, lastMouse, style) {
+    this.initialMouse = initialMouse;
+    this.style = style;
+    this.lastMouse = lastMouse;
+  }
+
+  draw(ctx) {
+    ctx.beginPath();
+    ctx.rect(
+      this.initialMouse.x,
+      this.initialMouse.y,
+      this.lastMouse.x - this.initialMouse.x,
+      this.lastMouse.y - this.initialMouse.y
+    );
+    ctx.strokeStyle = this.style.color;
+    ctx.lineWidth = this.style.width;
+    ctx.stroke();
+  }
+}
